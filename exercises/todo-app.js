b9089142030610e9304f60996d6a2d6f2a4add99
fs-nathan <nathan.makeitpro@gmail.com>
2026-07-30 21:13:35 +0700

    update todo-list exercise

--- exercises/todo-app.js
+++ exercises/todo-app.js
@@ -2,64 +2,73 @@ let items = [];
 
 getDataFromLocalStorage();
 
-function getDataFromLocalStorage () {
-	const itemsFromLocalStorage = localStorage.getItem('list')
-	if (itemsFromLocalStorage) {
-		items = JSON.parse(itemsFromLocalStorage)
-	}
+// render ol items
+function renderOlItems() {
+  const list = document.getElementById("list-items");
+  list.innerHTML = "";
+  items.forEach((item) => {
+    insertListItem(item);
+  });
 }
 
-
-function updateDataIntoLocalStorage () {
-	localStorage.setItem('list', JSON.stringify(items))
+function getDataFromLocalStorage() {
+  const itemsFromLocalStorage = localStorage.getItem("list");
+  if (itemsFromLocalStorage) {
+    items = JSON.parse(itemsFromLocalStorage);
+  }
 }
 
+function updateDataIntoLocalStorage() {
+  localStorage.setItem("list", JSON.stringify(items));
+}
 
-function onCompleted (e) {
-	const checked = e.target.checked;
-	const itemId = e.target.attributes['data-id'].value;
+function onCompleted(e) {
+  const checked = e.target.checked;
+  const itemId = e.target.attributes["data-id"].value;
 
-	const itemNameElement = document.getElementById(itemId);
+  const itemNameElement = document.getElementById(itemId);
 
-	if (checked) {
-		itemNameElement.classList.add('strike-through');
-	} else {
-		itemNameElement.classList.remove('strike-through');
-	}
-
+  if (checked) {
+    itemNameElement.classList.add("strike-through");
+  } else {
+    itemNameElement.classList.remove("strike-through");
+  }
 }
 
 function insertListItem(item) {
- /* update DOM ol */
+  /* update DOM ol */
 
-	const li = document.createElement('li');
-	li.innerHTML = `<span class="item-name" id="${item.id}">${item.name} </span><span class="actions"><input class="checkbox" type="checkbox" onchange="onCompleted(event)" data-id="${item.id}"/><i class="fas fa-trash fa-lg" data-id="${item.id}" onclick="onItemDelete(event)"></i></span>`;
+  const li = document.createElement("li");
+  li.innerHTML = `<span class="item-name" id="${item.id}">${item.name} </span><span class="actions"><input class="checkbox" type="checkbox" onchange="onCompleted(event)" data-id="${item.id}"/><i class="fas fa-trash" data-id="${item.id}" onclick="onItemDelete(event)"></i></span>`;
 
-	const list = document.getElementById('list-items');
-	list.append(li);
+  const list = document.getElementById("list-items");
+  list.append(li);
 }
 
-function onItemAdd (e) {
-	if (e.key == 'Enter') {
+function onItemAdd(e) {
+  if (e.key == "Enter") {
+    const newItem = {
+      name: e.target.value,
+      completed: false,
+      id: new Date().getTime(),
+    };
 
-		const newItem = {name: e.target.value, completed: false, id: new Date().getTime()}
+    items.push(newItem);
+    updateDataIntoLocalStorage();
 
-		items.push(newItem);
-		updateDataIntoLocalStorage();
+    insertListItem(newItem);
 
-		insertListItem(newItem);
-
-		e.target.value = '';
-	}
+    e.target.value = "";
+  }
 }
 
-function onItemDelete (e) {
-	const itemId = e.target.attributes['data-id'].value;
+function onItemDelete(e) {
+  const itemId = e.target.attributes["data-id"].value;
 
-	/* remove from items and update localStorage */
-	items = items.filter(item => item.id != itemId);
-	updateDataIntoLocalStorage()
+  /* remove from items and update localStorage */
+  items = items.filter((item) => item.id != itemId);
+  updateDataIntoLocalStorage();
 
+  /* remove from DOM */
 
-	/* remove from DOM */
 }
